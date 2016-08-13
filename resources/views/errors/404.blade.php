c0551b98381dc389072a1b6e54f793cccadac846
SGolenzovskiy <Golenzovskiy@gmail.com>
2016-08-13 19:40:18 +0300

    Добавлена страница "ошибка 404" #15

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" media="all" href="/css/error_style.css">
+    <link rel="stylesheet" type="text/css" media="all" href="/css/ani-red.css">
+    <title>Ошибка 404 | 404 Not Found</title>
+    <script type="text/javascript" src="/js/modernizr.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="ani">
+        <div class="a1"><span><b></b></span></div>
+        <div class="a2"><span><b></b></span></div>
+        <div class="a3"><span><b></b></span></div>
+        <div class="a4"><span><b></b></span></div>
+        <div class="a5"><span><b></b></span></div>
+        <div class="a6"><span><b></b></span></div>
+        <div class="a7"><span><b></b></span></div>
+        <div class="a8"><span><b></b></span></div>
+        <div class="a9"><span><b></b></span></div>
+        <div class="a10"><span><b></b></span></div>
+    </div>
+    <div role="main" class="home">
+        <div>
+            <h1>Ошибка 404</h1>
+            <p class="intro">Перейти на <a href="/">главную страницу</a></p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
