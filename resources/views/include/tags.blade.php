0df5bcdea71e83803dcf9bc99d5110521f4c3f2a
SG <Golenzovskiy@gmail.com>
2016-09-28 22:49:53 +0300

    Небольшая доработка #21

--- resources/views/include/tags.blade.php
+++ resources/views/include/tags.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 {{ (isset($className)) ? $className : '' }}">
-<h4>Теги</h4>
+<h4>Теги <a href="/dictionarys#tags" class="btn btn-xs btn-link pull-right"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a></h4>
 <div class="panel panel-default" id="tag-panel">
     <div class="panel-body">
         <div class="row">
