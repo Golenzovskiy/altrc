e2b97dddef47a25dcff46b26811ad55210bd6759
SG <Golenzovskiy@gmail.com>
2016-10-06 00:27:00 +0300

    #32

---
+++ database/migrations/2016_10_05_211103_field_review_project.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FieldReviewProject extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function ($table) {
+            $table->string('review')->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
